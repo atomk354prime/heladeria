5a4a988cad419c06293f22f7bff7a48cc855bd96
atomk354prime <atomk354@gmail.com>
2025-09-29 21:34:19 -0500

    MalteadaManager.java

---
+++ MalteadaManager.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class MalteadaManager {
+    private ArrayList<Malteada> listaMalteadas;
+
+    public MalteadaManager() {
+        this.listaMalteadas = new ArrayList<>();
+    }
+
+    public void agregarMalteada(Malteada m) {
+        this.listaMalteadas.add(m);
+    }
+
+    public Malteada buscarMalteadaPorId(int id) {
+        for (Malteada m : listaMalteadas) {
+            if (m.id == id) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    public void eliminarMalteada(int id) {
+        Malteada m = buscarMalteadaPorId(id);
+        if (m != null) {
+            listaMalteadas.remove(m);
+        }
+    }
+
+    public ArrayList<Malteada> getListaMalteadas() {
+        return listaMalteadas;
+    }
+}
