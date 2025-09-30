b927ec5f58cdd48dacb4812324990fd152f2b924
atomk354prime <atomk354@gmail.com>
2025-09-29 21:35:33 -0500

    ToppingManager.java

---
+++ ToppingManager.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ToppingManager {
+    private ArrayList<String> listaToppings;
+
+    public ToppingManager() {
+        this.listaToppings = new ArrayList<>();
+    }
+
+    public void agregarTopping(String topping) {
+        this.listaToppings.add(topping);
+    }
+
+    public void eliminarTopping(String topping) {
+        this.listaToppings.remove(topping);
+    }
+
+    public ArrayList<String> getListaToppings() {
+        return this.listaToppings;
+    }
+}
